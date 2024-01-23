c25f47315dcec582c824430ac974f511625e3f1e
k3ybladewielder <alyssonalk@gmail.com>
2024-01-23 16:39:47 -0300

    update demo

--- demo.py
+++ demo.py
@@ -4,29 +4,52 @@ import plotly.express as px
 
 df_plot = pd.read_parquet('./gold/df_forecast_results.parquet') #, parse_dates=['timestamp'])
 
-import gradio as gr
-
 # Função para plotar o gráfico com base nos parâmetros fornecidos
 def plot_graph(ticker_options):
-    filtered_df = df_plot[(df_plot['ticker'].isin(ticker_options))] # & (df_plot['month'].isin(months))]
-    
-    fig = px.line(filtered_df, x=filtered_df["month"], y=filtered_df['close_mean'], title=f'Close Mean Over Time')
-    fig.update_layout(xaxis_title='Date', yaxis_title='Close Mean')
-    
+    df_ticker = df_plot[df_plot["ticker"].isin(ticker_options)]
+
+    fig = px.line()
+    for ticker in df_ticker["ticker"].unique():
+        # Adicionar linha para HIST e PRED
+        fig.add_scatter(x=df_ticker.index, 
+                        y=df_ticker["close_mean"],
+                        mode='lines', name=f'{ticker}',
+                        line=dict(dash='solid'),  # Linha HIST
+                        legendgroup=f'{ticker}')
+
+        fig.add_scatter(x=df_ticker[df_ticker["type"] == "PRED"].index,
+                        y=df_ticker[df_ticker["type"] == "PRED"]["close_mean"],
+                        mode='lines',
+                        name=f'{ticker}',
+                        line=dict(dash='dot'),  # Linha PRED
+                        legendgroup=f'{ticker}')
+
+    # Adicionar rótulos e título
+    fig.update_layout(
+        title="Histórico e Previsão para os Tickers",
+        xaxis_title="Data",
+        yaxis_title="Valor",
+        )        
     return fig
 
 # Definir as opções para a caixa de seleção
 ticker_options = list(df_plot['ticker'].unique())
-months = list(df_plot["month"].unique())
+months = list(df_plot.index)
+
+# Criar a interface Gradio com blocos
+#intro_block = gr.Markdown("Time Series Forecasting of Cryptocurrencies.")
+ticker_block = gr.CheckboxGroup(ticker_options, label='Selecione o Ticker', every=True)
+graph_block = gr.Plot(plot_graph(ticker_options))
 
 # Criar a interface Gradio
-iface = gr.Interface(fn=plot_graph, inputs=[
-                         gr.CheckboxGroup(ticker_options, label='Select Ticker', info="Ticker to predict the next 6 months"),
-                         #gr.DatePicker(label='Select Start Date', default='2018-01-01')
-                         #gr.Dropdown(months, value=months, multiselect=True, label="Datas")
-                        ],
-                     outputs=gr.Plot(plot_graph(ticker_options))
-                     )
+iface = gr.Interface(fn=plot_graph,
+                    title="Time Series Forecasting of Cryptocurrencies",
+                    description="Forecast dos top 20 criptoativos em captalização de mercado. \
+                                 Sendo eles ```ADA, AVAX, BCH, BNB, BTC, DAI, DOGE, DOT, ETH, ICP, LINK, LTC, MATIC, SHIB, SOL, TON, TRX, USDC, USDT, XRP```. \
+                                 Os dados utilizados foram coletados no ```Coinmarketcap```.",
+                                 inputs=[ticker_block],
+                                 outputs=graph_block
+                            )
 
 # Lançar a interface
 iface.launch()
